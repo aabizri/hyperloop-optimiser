3dcbb60a8d4f3dfbd76a4c9d0da4a2a89963ae44
Alexandre A. Bizri <alexandre@bizri.fr>
2017-03-24 21:47:00 +0100

    Modularise buildGraph and better its documentation and error handling

--- buildGraph.go
+++ buildGraph.go
@@ -6,17 +6,14 @@ import (
 	"strconv"
 )
 
-func (input *Input) buildGraph() (goraph.Graph, error) {
-
-	// Create a new graph
-	graph := goraph.NewGraph()
-
+// listCities lists all the cities implied in an Input
+func (input *Input) listCities() ([]uint, error) {
 	// List all the unique cities
 	var cities []uint = make([]uint, input.CitiesCount)
 	for _, co := range input.CostOffers {
 		// Sanity check
 		if co.From == co.To {
-			return nil, fmt.Errorf("From (%d) is equal to to (%d)", co.From, co.To)
+			return nil, fmt.Errorf("listCities: \"from\" (%d) is equal to \"to\" (%d)", co.From, co.To)
 		}
 		var (
 			fromIsUnique bool = true
@@ -43,6 +40,29 @@ func (input *Input) buildGraph() (goraph.Graph, error) {
 			cities = append(cities, co.To)
 		}
 	}
+	return cities, nil
+}
+
+// addEdgeToGraph adds an edge to a graph given a Costoffer and a graph to add to (duh)
+func (co CostOffer) addEdgeToGraph(g goraph.Graph) error {
+	err := g.AddEdge(goraph.NewNode(strconv.FormatUint(uint64(co.From), 10)).ID(), goraph.NewNode(strconv.FormatUint(uint64(co.To), 10)).ID(), float64(co.Price))
+	if err != nil {
+		return fmt.Errorf("addEdgeToGraph: error: %v", err)
+	}
+	return err
+}
+
+// buildGraph transforms an Input to a graph
+func (input *Input) buildGraph() (goraph.Graph, error) {
+
+	// Create a new graph
+	graph := goraph.NewGraph()
+
+	// Get the cities
+	cities, err := input.listCities()
+	if err != nil {
+		return graph, fmt.Errorf("buildGraph: error: %v", err)
+	}
 
 	// Add the nodes
 	for _, city := range cities {
@@ -50,15 +70,15 @@ func (input *Input) buildGraph() (goraph.Graph, error) {
 		graphNode := goraph.NewNode(cityStr)
 		ok := graph.AddNode(graphNode)
 		if ok != true {
-			logger.Printf("Tried to add node %s to the graph but it appears it already exists", cityStr)
+			logger.Printf("buildGraph: Tried to add node %s to the graph but it appears it already exists", cityStr)
 		}
 	}
 
 	// Now let's deal with the edges
 	for _, co := range input.CostOffers {
-		err := graph.AddEdge(goraph.NewNode(strconv.FormatUint(uint64(co.From), 10)).ID(), goraph.NewNode(strconv.FormatUint(uint64(co.To), 10)).ID(), float64(co.Price))
+		err = co.addEdgeToGraph(graph)
 		if err != nil {
-			return graph, err
+			return graph, fmt.Errorf("buildGraph: error: %v", err)
 		}
 	}
 
