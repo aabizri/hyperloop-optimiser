97480cc5f5e0dc4a50d2912dcd11c82fc189e23f
Alexandre A. Bizri <alexandre@bizri.fr>
2017-03-25 01:04:31 +0100

    Add tests for listCities

---
+++ buildGraph_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+)
+
+const iterations int = 360
+
+// Test listCities under a diversity of inputs
+func TestListCities_Rand(t *testing.T) {
+	for i := 0; i <= iterations; i++ {
+		input := helperGenInput(0, 32, 0, 64, 120, 360)
+		if _, err := input.listCities(); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+// Test listCities under a known input
+func TestListCities_Known(t *testing.T) {
+	// Known cities list
+	knownCitiesList := []uint{0, 1, 2, 3}
+	funcCitiesList, err := knownInput.listCities()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Compare one to one
+	if len(knownCitiesList) != len(funcCitiesList) {
+		t.Fatalf("Length of known cities list (%d) differs from that of func (%d)", len(knownCitiesList), len(funcCitiesList))
+	}
+}
